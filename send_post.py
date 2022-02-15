6d4305c758c94869657baa8eb2f5bb5505691c9f
gkslsahin <gkslshn26@gmail.com>
2022-02-15 14:14:57 +0300

    add send post

---
+++ send_post.py
@@ -0,0 +1,14 @@
+import select
+
+import psycopg2
+import requests
+import json
+
+url = 'http://127.0.0.1:5000/db_data'
+
+headers = {'Content-Type': 'application/json'}
+myobj = {'somekey': 'somevalue'}
+
+
+
+
